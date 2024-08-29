0e1e27c16645111d6b6ad57ccbc3ac11d5f1386f
Wesley Santos <wesleysantos103094@gmail.com>
2024-08-28 21:01:40 -0300

    Atualização no componente nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,14 +1,13 @@
 <div class="container-fluid">
     <!-- Navbar brand -->
     <a class="navbar-brand" target="_blank" href="https://mdbootstrap.com/docs/standard/">
-      <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="16" alt="" loading="lazy"
-        style="margin-top: -3px;" />
+      <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="16" alt="" loading="lazy" style="margin-top: -3px;" />
     </a>
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
-      aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
       <i class="fas fa-bars"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarExample01">
+      <!-- Links de navegação -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item active">
           <a class="nav-link" aria-current="page" href="{{route('home')}}">Home</a>
@@ -16,18 +15,31 @@
         <li class="nav-item">
           <a class="nav-link" href="{{ route('posts') }}" rel="nofollow">Posts</a>
         </li>
- @guest
+        @guest
         <li class="nav-item">
-        <a class="nav-link" href="{{route('login')}}">Login</a>
+          <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
- @endguest
+        @endguest
       </ul>
 
-      Bem vindo
-      @if (auth()->guest())
-      Visitante
-      @else
-      {{ auth()->user()->fullName }} | <a href="{{ route('logout') }}">Logout</a>
-      @endif
+      <!-- Centralização do formulário de pesquisa -->
+      <div class="d-flex justify-content-center flex-grow-1">
+        <form class="d-flex" action="{{ route('home')}}" method="get" style="max-width: 400px;">
+          <input class="form-control me-2" type="text" name="s" placeholder="O que deseja buscar?" value="{{ request()->input('s') ?? '' }}">
+          <button class="btn btn-dark" type="submit">Buscar</button>
+        </form>
+      </div>
+
+      <!-- Informações do usuário -->
+      <span class="navbar-text ms-3">
+        Bem-vindo,
+        @if (auth()->guest())
+        Guest
+        @else
+        {{ auth()->user()->fullName }} |
+        <a href="{{ route('logout') }}" class="btn btn-outline-dark btn-sm ms-2">Logout</a>
+        @endif
+      </span>
     </div>
   </div>
+
